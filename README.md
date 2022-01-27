8f6c5d0c81f3c7520f365f217d4a56731a511ad3
migs <migs@titter.biz>
2022-01-27 16:41:09 -0600

    Initial Commit, in creative state

---
+++ README.md
@@ -0,0 +1,60 @@
+# `s3-deploy`
+
+## Yeet your static site to the cloud
+
+```shell
+∫ s3-deploy help
+s3-deploy 1.0
+olmigs <migs@mdguerrero.com>
+Deploy your static site to AWS S3
+
+USAGE:
+    s3-deploy <SUBCOMMAND>
+
+OPTIONS:
+    -h, --help       Print help information
+    -V, --version    Print version information
+
+SUBCOMMANDS:
+    help        Print this message or the help of the given subcommand(s)
+    modified    Print recently modified files (< 24 hrs) in <PROJECT>
+    print       Print objects in <BUCKET>
+    yolo        Deploy recently modified files in <PROJECT> to <BUCKET>
+∫
+```
+
+For a more philosophical dissection of the tool, [see this blog post](http://mdguerrero.com/blog).
+
+### Requirements
+
+Any static assets you wish to deploy from a root directory e.g. `project` must
+
+1. live in `project/public`, and
+2. be specified in an array in `project/out/public.json`.
+
+`s3-deploy` assumes you have AWS access keys are available in your environment. See [Getting started](https://docs.aws.amazon.com/sdk-for-rust/latest/dg/getting-started.html#getting-started-step2).
+
+### Usage
+
+```shell
+∫ s3-deploy modified -p /Static/site/project
+
+2 files modified recently:
+   build/bundle.js
+   index.html
+∫ s3-deploy yolo -b example.com -p /Static/site/project -s app
+
+Upload success for app/build/bundle.js
+  Entity tag "<REDACTED>"
+Upload success for app/index.html
+  Entity tag "<REDACTED>"
+∫
+```
+
+### Notices
+
+-   Probably does not work on Windows
+-   `Commands::Upload` is under-implemented
+-   `Commands::Delete` is unimplemented
+
+**Feel free to raise an issue for any of these, or other, concerns.**
---
+++ src/main.rs
@@ -0,0 +1,230 @@
+#![allow(dead_code)]
+use aws_sdk_s3::{ByteStream, Client, Error};
+use clap::{Parser, Subcommand};
+use mime::Mime;
+use std::{collections::HashMap, fs, path::Path};
+
+fn get_out_files(path: &str) -> Vec<String> {
+    let filename = path.to_owned() + "/out/public.json";
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let json: Vec<String> = serde_json::from_str(&contents).expect("It's not an array somehow");
+    json
+}
+
+fn get_modified_files(path: &String) -> HashMap<String, String> {
+    let in_filenames = get_out_files(path);
+    let mut out_filenames = HashMap::new();
+    for name in in_filenames {
+        let filepath = path.clone() + "/public/" + &name;
+        let metadata = fs::metadata(&filepath).expect("Error getting metadata");
+        let last_modified = metadata
+            .modified()
+            .expect("Error getting system time")
+            .elapsed()
+            .expect("Error getting elapsed")
+            .as_secs();
+        if last_modified < 24 * 3600 && metadata.is_file() {
+            out_filenames.insert(name, filepath);
+        }
+    }
+    out_filenames
+}
+
+fn get_mime_type(filename: &str) -> Result<Mime, &'static str> {
+    let parts: Vec<&str> = filename.split('.').collect();
+    let res = match parts.last() {
+        Some(v) => match *v {
+            "css" => mime::TEXT_CSS,
+            "html" => mime::TEXT_HTML,
+            "png" => mime::IMAGE_PNG,
+            "js" => mime::APPLICATION_JAVASCRIPT,
+            "jpg" => mime::IMAGE_JPEG,
+            "json" => mime::APPLICATION_JSON,
+            "map" => mime::APPLICATION_OCTET_STREAM,
+            "svg" => mime::IMAGE_SVG,
+            &_ => mime::TEXT_PLAIN,
+        },
+        None => mime::TEXT_PLAIN,
+    };
+    Ok(res)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_out_files_len() {
+        let files = get_out_files("/Some/absolute/path");
+        assert_eq!(files.len(), 7);
+    }
+
+    #[test]
+    fn test_mime_types() {
+        let files = get_out_files("/Some/absolute/path");
+        for file in files {
+            let res = get_mime_type(&file);
+            assert_eq!(res.is_ok(), true);
+        }
+    }
+}
+
+async fn show_objects(client: &Client, bucket: &str) -> Result<(), Error> {
+    let resp = client.list_objects_v2().bucket(bucket).send().await?;
+    println!("{}", bucket);
+    for object in resp.contents().unwrap_or_default() {
+        println!("   {}", object.key().unwrap_or_default());
+    }
+    Ok(())
+}
+
+async fn upload_object(
+    client: &Client,
+    bucket: &str,
+    mime: mime::Mime,
+    filename: &str,
+    key: &str,
+) -> Result<(), Error> {
+    let body = ByteStream::from_path(Path::new(filename)).await;
+    match body {
+        Ok(b) => {
+            let resp = client
+                .put_object()
+                .content_type(mime.to_string())
+                .bucket(bucket)
+                .key(key)
+                .body(b)
+                .send()
+                .await?;
+            match resp.e_tag {
+                Some(tag) => println!("Upload success for {} \n  Entity tag {}", key, tag),
+                None => println!("Upload success for {}", key),
+            }
+        }
+        Err(e) => {
+            println!("Got an error uploading {} : {}", key, e);
+        }
+    }
+    Ok(())
+}
+
+async fn print(bucket: &String, client: &Client) -> () {
+    match show_objects(client, bucket).await {
+        Err(e) => println!("{}", e),
+        _ => {}
+    }
+}
+
+async fn deploy(
+    bucket: &String,
+    project: &String,
+    prepend: &Option<String>,
+    client: &Client,
+) -> Result<(), Error> {
+    let key_folder = match prepend {
+        Some(pre) => {
+            let mut name = pre.clone();
+            name.push_str("/");
+            name
+        }
+        None => String::from(""),
+    };
+    let modified_files = get_modified_files(project);
+    for (key, filename) in modified_files {
+        let mime = get_mime_type(&filename).expect("Could not get media type");
+        let mut full_key = key_folder.clone();
+        full_key.push_str(&key);
+        // println!("{}", &full_key);
+        // uncomment below to actually upload!
+        upload_object(client, bucket, mime, &filename, &full_key).await?;
+    }
+    Ok(())
+}
+
+async fn deploy_single(
+    bucket: &String,
+    file: &String,
+    prepend: &Option<String>,
+    client: &Client,
+) -> Result<(), Error> {
+    // get key from file name
+    // get MIME type
+    // get prepend
+    // upload asset
+    println!("todo!");
+    Ok(())
+}
+
+fn modified(project: &String) -> () {
+    let modified_files = get_modified_files(project);
+    println!("{} files modified recently: ", modified_files.len());
+    for (key, _value) in modified_files {
+        println!("   {}", key);
+    }
+}
+
+#[derive(Parser)]
+#[clap(name = "s3-deploy")]
+#[clap(author = "olmigs <migs@mdguerrero.com>")]
+#[clap(version = "1.0")]
+#[clap(about = "Deploy your static site to AWS S3", long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Print objects in <BUCKET>
+    Print {
+        #[clap(short, long)]
+        bucket: String,
+    },
+    /// Deploy recently modified files in <PROJECT> to <BUCKET>
+    Yolo {
+        #[clap(short, long)]
+        bucket: String,
+        #[clap(short, long)]
+        project: String,
+        #[clap(short, long)]
+        subdirectory: Option<String>,
+    },
+    /// Print recently modified files (< 24 hrs) in <PROJECT>
+    Modified {
+        #[clap(short, long)]
+        project: String,
+    },
+    /// Deploy <FILE> to <BUCKET>
+    Upload {
+        #[clap(short, long)]
+        bucket: String,
+        #[clap(short, long)]
+        file: String,
+        #[clap(short, long)]
+        subdirectory: Option<String>,
+    },
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let cli = Cli::parse();
+    let shared_config = aws_config::load_from_env().await;
+    let client = Client::new(&shared_config);
+    println!();
+
+    match &cli.command {
+        Commands::Print { bucket } => print(bucket, &client).await,
+        Commands::Yolo {
+            bucket,
+            project,
+            subdirectory,
+        } => deploy(bucket, project, subdirectory, &client).await?,
+        Commands::Modified { project } => modified(project),
+        Commands::Upload {
+            bucket,
+            file,
+            subdirectory,
+        } => deploy_single(bucket, file, subdirectory, &client).await?,
+    }
+    Ok(())
+}
